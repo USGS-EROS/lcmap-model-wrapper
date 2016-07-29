dc44666e70e1f37c353c5b5c1331f4c834e4be7b
Jonathan Morton <jonathan.morton.ctr@usgs.gov>
2016-07-29 14:01:19 -0500

    Update example CLI wrapper

--- src/python/cli.py
+++ src/python/cli.py
@@ -1,8 +1,17 @@
+#
+# This is the starting point of all CLI wrappers.
+#
+
 import click
 import json
 import index
-from lcmap.client
+from lcmap.client import Client
 
+#
+# A wrapper uses the LCMAP REST API Python Client to
+# retrieve and persist model results.
+#
+client = Client()
 
 @click.group()
 def cli():
@@ -11,14 +20,12 @@ def cli():
 @click.command()
 @click.option('--point')
 @click.option('--time')
-def ndvi(point, time):
+def echo(point, time):
     result = {'point':point,'time':time}
     click.echo(json.dumps(result))
     return result
 
-
-cli.add_command(ndvi)
-
+cli.add_command(echo)
 
 if __name__ == '__main__':
     cli()
